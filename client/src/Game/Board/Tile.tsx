07c766f3e945e6d50063370776e1ce6f91d51407
Vlad V <vvcodeweb@gmail.com>
2022-09-17 20:49:17 +0300

    Added tile tooltip

--- client/src/Game/Board/Tile.tsx
+++ client/src/Game/Board/Tile.tsx
@@ -16,7 +16,7 @@ import {
 import waterImg from "components/../../public/water.jpg";
 import Ship from "Game/ShipDocks/Ship";
 import { TileType } from "Game/Board/types";
-import { getShipPartByIdx, getSize } from "utils";
+import { getLetterByX, getShipPartByIdx, getSize } from "utils";
 import { BoardContext } from "Game/Board/context/BoardContext";
 import shellImg from "components/../../public/shell.png";
 //import fireImg from "components/../../public/fire2.png";
@@ -24,6 +24,7 @@ import fireImg from "components/../../public/test.gif";
 import blockImg from "components/../../public/block.png";
 import useGetGameContext from "Game/hooks/useGetGameContext";
 import useStyles from "hooks/useStyle";
+import { Tooltip, Zoom } from "@mui/material";
 
 const Tile = ({ tile, inDev }: { tile: TileType; inDev?: boolean }) => {
   const styles = useStyles();
@@ -161,36 +162,49 @@ const Tile = ({ tile, inDev }: { tile: TileType; inDev?: boolean }) => {
   };
   //TODO: add proper type check for occupied by
   return (
-    <Grid2
-      onClick={onClick}
-      ref={drop}
-      xs={1}
-      className={tile.enemy ? `${TILE}_enemy` : TILE}
-      style={tileStyle}
+    <Tooltip
+      placement="top"
+      TransitionComponent={Zoom}
+      followCursor
+      enterNextDelay={600}
+      arrow
+      title={
+        <div style={{ pointerEvents: "none" }}>
+          {getLetterByX(tile.x)}:{tile.y + 1}
+        </div>
+      }
     >
-      <RenderShell />
-      {inDev && <>DEV</>}
-      {tile.occupiedBy &&
-        tile.occupiedBy !== ENEMY_SHIP &&
-        tile.occupiedBy.part ===
-          getShipPartByIdx(getSize(tile.occupiedBy) - 1) && (
-          <div
-            style={{
-              position: "absolute",
-              top:
-                tile.occupiedBy.orientation === HORIZONTAL
-                  ? styles.distanceShipTile
-                  : 0,
-              left:
-                tile.occupiedBy.orientation === VERTICAL
-                  ? styles.distanceShipTile
-                  : 0,
-            }}
-          >
-            <Ship ship={tile.occupiedBy} />
-          </div>
-        )}
-    </Grid2>
+      <Grid2
+        onClick={onClick}
+        ref={drop}
+        xs={1}
+        className={tile.enemy ? `${TILE}_enemy` : TILE}
+        style={tileStyle}
+      >
+        <RenderShell />
+        {inDev && <>DEV</>}
+        {tile.occupiedBy &&
+          tile.occupiedBy !== ENEMY_SHIP &&
+          tile.occupiedBy.part ===
+            getShipPartByIdx(getSize(tile.occupiedBy) - 1) && (
+            <div
+              style={{
+                position: "absolute",
+                top:
+                  tile.occupiedBy.orientation === HORIZONTAL
+                    ? styles.distanceShipTile
+                    : 0,
+                left:
+                  tile.occupiedBy.orientation === VERTICAL
+                    ? styles.distanceShipTile
+                    : 0,
+              }}
+            >
+              <Ship ship={tile.occupiedBy} />
+            </div>
+          )}
+      </Grid2>
+    </Tooltip>
   );
 };
 
